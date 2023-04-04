748ade7f5b91087794c4963a7ed2f53a0bc3fa31
RamjanAli <ramjanali@erevive.in>
2023-04-04 11:58:14 +0000

    Add Doctype

---
+++ server_manager/server_manager/doctype/server/server.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, RamjanAli Lal and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Server(Document):
+	pass
